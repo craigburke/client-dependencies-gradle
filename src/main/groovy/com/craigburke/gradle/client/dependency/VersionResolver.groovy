332f96e2c5f00f4d89d7fc6479f44f46b324959a
Craig Burke <craig@craigburke.com>
2016-02-11 16:12:44 -0500

    Combined expression and hyphen ranges versions are now resolved

--- src/main/groovy/com/craigburke/gradle/client/dependency/VersionResolver.groovy
+++ src/main/groovy/com/craigburke/gradle/client/dependency/VersionResolver.groovy
@@ -4,12 +4,16 @@ import java.util.regex.Pattern
 
 class VersionResolver {
 
-    private static final Pattern EQUALS = ~/(?:\s*)?=?v?(\d*\.\d*\.\d*.*)?/
-    private static final Pattern LESS_THAN = ~/(?:\s*)(?:<)(?:\s*)?v?(\d*\.\d*\.\d*.*)?/
-    private static final Pattern GREATER_THAN = ~/(?:\s*)(?:>)(?:\s*)?v?(\d*\.\d*\.\d*.*)?/
-    private static final Pattern LESS_THAN_EQUAL = ~/(?:\s*)(?:<=)(?:\s*)?v?(\d*\.\d*\.\d*.*)?/
-    private static final Pattern GREATER_THAN_EQUAL = ~/(?:\s*)(?:>=)(?:\s*)?v?(\d*\.\d*\.\d*.*)?/
-    private static final Pattern CARET_RANGE = ~/(?:\s*)(?:\^)(?:\s*)?v?(\d*\.\d*\.\d*.*)?/
+    private static final String VERSION_GROUP = /v?(\d*\.\d*\.\d*[^\s]*)/
+    private static final String SPACES = /(?:\s*)/
+
+    private static final Pattern EQUALS = ~/=?$SPACES$VERSION_GROUP/
+    private static final Pattern LESS_THAN = ~/(?:<)$SPACES$VERSION_GROUP/
+    private static final Pattern GREATER_THAN = ~/(?:>)$SPACES$VERSION_GROUP/
+    private static final Pattern LESS_THAN_EQUAL = ~/(?:<=)$SPACES$VERSION_GROUP/
+    private static final Pattern GREATER_THAN_EQUAL = ~/(?:>=)$SPACES$VERSION_GROUP/
+    private static final Pattern CARET_RANGE = ~/(?:\^)$SPACES$VERSION_GROUP/
+    private static final Pattern HYPHEN_RANGE = ~/$VERSION_GROUP$SPACES\-$SPACES$VERSION_GROUP/
 
     static Version resolve(String expression, List<Version> versions) {
         List<Version> sortedVersions = versions.toSorted { v1, v2 -> v2 <=> v1 }
@@ -17,6 +21,12 @@ class VersionResolver {
     }
 
     static boolean matches(Version version, String expression) {
+        expression.tokenize('||')*.trim().any { String simpleExpression ->
+            matchesSimpleExpression(version, simpleExpression)
+        }
+    }
+
+    private static boolean matchesSimpleExpression(Version version, String expression) {
         boolean result = false
 
         if (expression ==~ EQUALS) {
@@ -42,6 +52,14 @@ class VersionResolver {
         else if (expression ==~ GREATER_THAN_EQUAL) {
             expression.find(GREATER_THAN_EQUAL) { String match, String versionExpression ->
                 result = version >= new Version(versionExpression)
+            }
+        }
+        else if (expression ==~ HYPHEN_RANGE) {
+            expression.find(HYPHEN_RANGE) { String match, String expression1, String expression2 ->
+                Version rangeBottom = new Version(expression1)
+                Version rangeTop = new Version(expression2)
+
+                result = (version >= rangeBottom && version <= rangeTop)
             }
         }
         else if (expression ==~ CARET_RANGE) {
