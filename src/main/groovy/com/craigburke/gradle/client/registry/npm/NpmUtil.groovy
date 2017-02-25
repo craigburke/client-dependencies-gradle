d2b6733f1f0d144b57a2d577974c9abf432ec6a8
Craig Burke <craig@craigburke.com>
2017-02-25 18:42:50 -0500

    Don't assume all npm dependencies are in a package subfolder. Fixes #72

--- src/main/groovy/com/craigburke/gradle/client/registry/npm/NpmUtil.groovy
+++ src/main/groovy/com/craigburke/gradle/client/registry/npm/NpmUtil.groovy
@@ -1,5 +1,7 @@
 package com.craigburke.gradle.client.registry.npm
 
+import groovy.io.FileType
+
 /**
  *
  * Util class for NPM
@@ -11,15 +13,17 @@ class NpmUtil {
     static void extractTarball(File sourceFile, File destination) {
         AntBuilder builder = new AntBuilder()
         builder.project.buildListeners.first().setMessageOutputLevel(0)
+
         builder.untar(src: sourceFile.absolutePath,  dest: destination.absolutePath,
-                compression: 'gzip', overwrite: true) {
-            patternset {
-                include(name: 'package/**')
-            }
-            mapper {
-                globmapper(from: 'package/*', to: '*')
+                compression: 'gzip', overwrite: true)
+
+        // move files and folders from a subdirectory (such as package) into the root of the destination folder
+        destination.eachFile(FileType.DIRECTORIES) { File rootDirectory ->
+            rootDirectory.eachFile { File file ->
+                file.renameTo(destination.absolutePath + File.separator + file.name)
             }
         }
+
     }
 
 }
